35c198e30cc8f36b2bf148b83fa7f1106b284b3b
Naveenkumar <nawinkumar94@gmail.com>
2020-10-03 09:35:42 +0530

    docker-file remove commit

--- infrastructure/terraform_build/install_terraform.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-echo "****************************"
-echo "** Downloading terraform Docker Image ***"
-echo "****************************"
-docker-compose -f terraform_build/docker-compose.yml build --no-cache
